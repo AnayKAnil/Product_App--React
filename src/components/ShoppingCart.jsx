e75a28f730003a2b03c535a854a007871ea628f5
Kuroha284 <anayanil698@gmail.com>
2024-12-29 19:42:38 +0530

    assignment done

---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,140 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            
+                        <div class="card">
+                            <img src="https://imgs.search.brave.com/k4kLpdF9qUgvqN-2qRI1BacNSxRm0_dsqRGV6Ef4mcM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL1Mv/YXBsdXMtbWVkaWEt/bGlicmFyeS1zZXJ2/aWNlLW1lZGlhL2Q3/Y2NiNDk5LWIxNWMt/NGFkMi1iOWRlLTg0/NzlmMWE1MTMxMC5f/X0NSMCwwLDgwMCw2/MDBfUFQwX1NYODAw/X1YxX19fLmpwZw" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">Dell Inspiron 15 3530</h5>
+                              <p class="card-text">₹53,500.00</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://imgs.search.brave.com/EUDwGPJZCx6yrNKU_vndogdPNzFi_dXwUHtYsBW_0so/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL1Mv/YXBsdXMtbWVkaWEt/bGlicmFyeS1zZXJ2/aWNlLW1lZGlhLzY1/NmYyNGNlLWY5ZTYt/NGY5NC05YWZmLWU1/YzkxZmY3OGY0Mi5f/X0NSMCwwLDYwMCw2/MDBfUFQwX1NYMzAw/X1YxX19fLmpwZw" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">Acer Aspire Lite</h5>
+                              <p class="card-text">₹59,000</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://m.media-amazon.com/images/I/71p-D5bOqJL._SX679_.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">Lenovo IdeaPad 3</h5>
+                              <p class="card-text">₹55,190</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://m.media-amazon.com/images/I/41K2SibytGL._SX300_SY300_QL70_FMwebp_.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">HP Pavilion</h5>
+                              <p class="card-text">₹77,142</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://m.media-amazon.com/images/I/710EaMlZrTL._SX679_.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">ASUS Vivobook 15</h5>
+                              <p class="card-text">₹80,990</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://m.media-amazon.com/images/I/618ac7UaMrL._SX679_.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">Lenovo IdeaPad Pro 5</h5>
+                              <p class="card-text">₹1,46,890</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://m.media-amazon.com/images/I/61keRkmSGBL._SX679_.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">Acer Smartchoice Aspire Lite</h5>
+                              <p class="card-text">₹47,990
+                              </p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://m.media-amazon.com/images/I/41IMroAyzVL._SX300_SY300_QL70_FMwebp_.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">Dell Smartchoice Core i3-1215U</h5>
+                              <p class="card-text">₹49,120</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://m.media-amazon.com/images/I/61fsBFww9DL._SX679_.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">ASUS Vivobook 16</h5>
+                              <p class="card-text">₹69,990</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+                            <img src="https://m.media-amazon.com/images/I/61w2NW9T78L._SX679_.jpg" class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                              <h5 class="card-title">HP Pavilion Aero AI</h5>
+                              <p class="card-text">₹91,544</p>
+                              <a href="#" class="btn btn-primary">Buy Now</a>
+                            </div>
+                          </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ShoppingCart
